7c3bb9c8a837683574d34d86b083d566426329bd
CozmmoIsIron <94890986+CozmmoIsIron@users.noreply.github.com>
2021-11-24 15:44:03 -0800

    Update calculatorproject.java

--- calculatorproject.java
+++ calculatorproject.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.*;
@@ -10,18 +11,10 @@ import java.lang.*;
 public class Main {
 
 
-
-
-	public static void main(String[] args) throws FileNotFoundException
-	{  startCalc(); // calling the start calc method
-
-
-
-
-
-
-
-
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("Welcome to my calculator");
+		startCalc(); // calling the start calc method
+		System.out.println("Goodbye");
 
 		// add a welcome message here
 
@@ -43,126 +36,126 @@ public class Main {
 		// add a goodbye message here
 
 	}
-	public static void startCalc(){
-		String Q="";
+
+	public static void startCalc() {
+		String Q;
+		String s;
+		Scanner answer = new Scanner(System.in);
 		// your code here to get user input, and calculate/print results. You'll call
 		// the calculate(String s) as part of your code here, which returns a String
 		// with the result to print.
-		System.out.println("Welcome to my calculator");
-		Scanner answer = new Scanner(System.in);
-		System.out.println("Enter an  expression remember use spaces");
-		String s = answer.nextLine();
-		if (s.equalsIgnoreCase("quit")) {  // credit: Pranav S. helped me with this part
-			System.out.println("Goodbye");
-		}
-		else {
-			if(calculate(s).equalsIgnoreCase("error")) {
-				System.out.println("ERROR");
+		do {
+			System.out.println("Enter an  expression remember use spaces");
+			 s = answer.nextLine();
+			 if(!(s.equalsIgnoreCase("quit"))){
+				 if (calculate(s).equalsIgnoreCase("error")) {
+					 System.out.println("ERROR");
+				 } else {
+					 System.out.println(s + " = " + calculate(s));
+				 }
 			}
-			else {
-				System.out.println(s + " = " + calculate(s));
-			}
-		}
+
+		} while (!(s.equalsIgnoreCase("quit")));
 
 	}
 
-	public static String calculate(String s){
+	public static String calculate(String s) {
 
-		int numberscanned = 0;
+		double numberscanned = 0;
 		double num1 = 0;
 		double num2 = 0;
-		int numberscanned2 = 0;
-		Scanner answer2= new Scanner(s);
+		Double numberscanned2;
+		Scanner answer2 = new Scanner(s);
 		String operation = "";
 
 
-		if (answer2.hasNextInt()|| answer2.hasNextDouble() ) {
+		if (answer2.hasNextDouble()) {
 
-			numberscanned = answer2.nextInt();
+			numberscanned = answer2.nextDouble();
 			num1 = numberscanned;
 			operation = answer2.next();
-			if (answer2.hasNextInt()) {
-
-				numberscanned2 = answer2.nextInt();
+			if (answer2.hasNextDouble()) {
+				numberscanned2 = answer2.nextDouble();
 				num2 = numberscanned2;
-			}
-
-			return TwoOperand(answer2,num1,num2,operation);
-		} else {
+			} else return "ERROR";
+             if (answer2.hasNext()) {
+             	return "ERROR";
+			 } else return TwoOperand(answer2, num1, num2, operation);
+		} else if (answer2.hasNext()) {
 			String symbol = answer2.next();
-			numberscanned = answer2.nextInt();
-			num1 = numberscanned;
-              /*  numberscanned2 = answer2.nextInt();
-                num2 = numberscanned2;*/
-
-			return OneOP(answer2, symbol, num1, num2);
+			if(answer2.hasNextDouble()) {
+				numberscanned = answer2.nextDouble();
+				num1 = numberscanned;
+              if(answer2.hasNext()){
+              	return "ERROR" ;
+			  }
+              else return OneOP(answer2, symbol, num1);
+			} else return "ERROR";
 		}
+		else return "ERROR";
 	}
-	public static String OneOP(Scanner answer2,String symbol,double num1,double num2) {
+
+	public static String OneOP(Scanner answer2,String symbol,double num1) {
 
 		String result="";
-		if (!(symbol.equals("v")||symbol.equals("|")|| symbol.equals("sin") || symbol.equals("cos") || symbol.equals("tan") )) {
-		String j ="error";
-		result=j;
+		if (!(symbol.equals("v")||symbol.equals("|")|| symbol.equals("s") || symbol.equals("c") || symbol.equals("tan") )) {
+		return "ERROR";
+
 		}
 		if (symbol.equals("v")){
 
 			double an= Math.sqrt(num1);
-			String baka= Double. toString(an);
-			result=baka;
+			return String.valueOf(an);
 		}
+		else
 		if (symbol.equals("|")) {
 			double an= Math.abs(num1);
-			String baka= Double.toString(an);
-			result=baka;
+			return String.valueOf(an);
 		}
-		if (symbol.equals("sin")) {
+		else
+		if (symbol.equals("s")) {
 			double an= Math.sin(num1);
-			String baka= Double.toString(an);
-			result=baka;
+			return String.valueOf(an);
 		}
-		if (symbol.equals("cos")) {
+		else
+		if (symbol.equals("c")) {
 			double an= Math.cos(num1);
-			String baka= Double.toString(an);
-			result=baka;
+			return String.valueOf(an);
 		}
-		if (symbol.equals("tan")) {
+		else
+		if (symbol.equals("t")) {
 			double an= Math.tan(num1);
-			String baka= Double.toString(an);
-			result=baka;
+			return String.valueOf(an);
 		}
-		return result;
+		else
+		return "ERROR";
 	}
 
 	public static String TwoOperand(Scanner answer2, double num1, double num2, String operation){
-		String majic="";
-
+		String majic;
 
 		if (operation.equals("+")) {
 			double an=num1 + num2;
-			String baka= Double. toString(an);
-			majic=baka;
-
+			majic= Double. toString(an);
+			return majic;
 		}
-		if (operation.equals("-")) {
+		else if (operation.equals("-")) {
 			double an=num1 - num2;
-			String baka= Double.toString(an);
-			majic=baka;
+			majic= Double.toString(an);
+			return majic;
 		}
 
-		if (operation.equals("/")) {
+		else if (operation.equals("/")) {
 			double an= num1/num2;
-			String baka= Double. toString(an);
-			majic=baka;
+			majic= Double. toString(an);
+			return majic;
 		}
-		if (operation.equals("*")) {
+		else if (operation.equals("*")) {
 			double an= num1*num2;
-			String baka= Double. toString(an);
-			majic=baka;
+			return Double. toString(an);
 
-		}
 
-		return majic;
+		} else return "ERROR";
 	}
 	// you add code here to take a String as a parameter, and return a String with the result
 
