b9a4f5844638ed2d59ceff59fbbd10beacb0870b
CozmmoIsIron <94890986+CozmmoIsIron@users.noreply.github.com>
2023-03-03 17:28:48 -0800

    Update calculatorproject.java

--- calculatorproject.java
+++ calculatorproject.java
@@ -2,7 +2,6 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.*;
@@ -16,24 +15,7 @@ public class Main {
 		startCalc(); // calling the start calc method
 		System.out.println("Goodbye");
 
-		// add a welcome message here
-
-		// comment out either startCalc() or testCalc() (just the method call below)
-		// based on how you want to run the project:
-		// uncomment startCalc() to call your method for an interactive calculator
-		// uncomment testCalc() to try out a bunch of
-
-		// startCalc();   // you have to write this method below
-		// it should ask the user for input and print
-		// results until the user enters "quit" to stop
-
-
-		// use this to validate your project (the calculator part, anyways)
-		// testCalc();     // testCalc will call a calculate(String s) method that you create
-		// as part of your overall project. This method will take the user's
-		// input, and return a String with the appropriate output.
-
-		// add a goodbye message here
+		
 
 	}
 
@@ -41,9 +23,7 @@ public class Main {
 		String Q;
 		String s;
 		Scanner answer = new Scanner(System.in);
-		// your code here to get user input, and calculate/print results. You'll call
-		// the calculate(String s) as part of your code here, which returns a String
-		// with the result to print.
+		
 		do {
 			System.out.println("Enter an  expression remember use spaces");
 			 s = answer.nextLine();
@@ -157,20 +137,7 @@ public class Main {
 
 		} else return "ERROR";
 	}
-	// you add code here to take a String as a parameter, and return a String with the result
-
-	// you'll probably call other methods (that you write) here to do work,
-	// like deciding if it's a 2-part or 3-part expression, or calculating the result of a
-	// 2-part expression or a 3-part expression
-
-
-
-	// add more methods here
-
-	//
-	// DO NOT MODIFY THE METHOD BELOW!!!
-	// YOU WILL CALL IT FROM MAIN TO TEST OUT YOUR OTHER CODE/METHODS
-	//
+	
 	public static void testCalc() throws FileNotFoundException{
 		ArrayList<String> problems = new ArrayList<>();
 		ArrayList<String> results = new ArrayList<>();
